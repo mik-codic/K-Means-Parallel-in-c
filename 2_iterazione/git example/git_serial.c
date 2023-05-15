6b5501653fde6a5add90a097ac4b95035eda325e
tripls <m.presutto.p@gmail.com>
2023-05-15 14:54:33 +0200

    added better k initialization

--- 2_iterazione/git example/git_serial.c
+++ 2_iterazione/git example/git_serial.c
@@ -8,19 +8,50 @@
 #include<errno.h>
 #include<string.h>
 
-#define MAX_ITERATIONS 50
-
 /* Definition of the constant */
+
+#define MAX_ITERATIONS 50
 #define numOfElements 150
 #define COLS 4
 #define numOfClusters 3
+
+
+typedef struct {
+    double x, y, z, w;
+} Point;
 
 double dataset[numOfElements][COLS];
 
 // int numOfClusters = 0;
 // int numOfElements = 0;
 // int num_of_processes = 0;
+int collection_of_index[3];
+// Function to calculate the distance between two 4D points
+double distance(Point a, Point b) {
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2) + pow(a.z - b.z, 2) + pow(a.w - b.w, 2));
+}
 
+// Function to find the point in the data_ that maximizes the sum of the distances to the current set of selected points
+Point find_best_point(Point *data_, int n_points, Point *selected_points, int n_selected_points) {
+    Point best_point = data_[0];
+    int index = 0;
+    double max_sum = 0;
+    for (int i = 0; i < n_points; i++) {
+        double sum = 0;
+        for (int j = 0; j < n_selected_points; j++) {
+            sum += distance(data_[i], selected_points[j]);
+        }
+        if (sum > max_sum) {
+            max_sum = sum;
+            best_point = data_[i];
+            index = i;
+        }
+    }            
+    printf("\nIndex: %d",index);
+    collection_of_index[n_selected_points] = index;
+
+    return best_point;
+}
 /* This function goes through that data points and assigns them to a cluster */
 void assign2Cluster(double k_sepal_len[], double k_sepal_wid[], double k_petal_len[], double k_petal_wid[], double recv_sepal_len[], double recv_sepal_wid[], double recv_petal_len[], double recv_petal_wid[], int assign[])
 {
@@ -161,25 +192,68 @@ int main(int argc, char *argv[])
         data_sepal_wid_points[i] = dataset[i][1];
         data_petal_len_points[i] = dataset[i][2];
         data_petal_wid_points[i] = dataset[i][3];
-        printf("-------------------------------------------\n");
-        printf("ALL POINTS\n");
-        printf("Point # %d: %f, %f, %f, %f\n",i , data_sepal_len_points[i], data_sepal_wid_points[i], data_petal_len_points[i], data_petal_wid_points[i]);
-        // assign all point to the cluster 0
+        // printf("-------------------------------------------\n");
+        // printf("ALL POINTS\n");
+        // printf("Point # %d: %f, %f, %f, %f\n",i , data_sepal_len_points[i], data_sepal_wid_points[i], data_petal_len_points[i], data_petal_wid_points[i]);
+        // // assign all point to the cluster 0
         // initial setting (i = index of points)
         k_assignment[i] = 0;
     }
 
-    // randomly select initial k-means point (4 coordinates)
+    // randomly select initial k-means point (4 coordinates)               
     time_t t;
     srand((unsigned) time(&t));
     // the 3 random points selected as centroids
     int random;
+    // storing the dataset in the structure
+    Point data_[numOfElements];
+
+    for(int i = 0;i<numOfElements;i++){
+        data_[i].x = dataset[i][0];
+        data_[i].y = dataset[i][1];
+        data_[i].z = dataset[i][2];
+        data_[i].w = dataset[i][3];
+        
+    }
+    printf("\nmy point %f",data_[0].x);
+    
+    int n_points = sizeof(data_) / sizeof(Point);
+    printf("\nn points = %d", n_points);
+    // Find the n points that maximize the sum of the distances between them
+    int n = 3;
+    Point selected_points[n];
+
+    selected_points[0] = data_[0]; // Start with an arbitrary point
+    for (int i = 1; i < n; i++) {
+        selected_points[i] = find_best_point(data_, n_points, selected_points, i);
+    }
+    for(int i= 0;i<3;i++){
+        printf("\nlist of index of the 3 most distanced point %d",collection_of_index[i]);
+    }
+    // Print the n points that maximize the sum of the distances between them
+    printf("The %d points that maximize the sum of the distances between them are:\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("(%f, %f, %f, %f)\n", selected_points[i].x,
+               selected_points[i].y,
+               selected_points[i].z,
+               selected_points[i].w);
+    }
+
+    int collection_init_index[3];
+    collection_init_index[0] = collection_of_index[0];
+    collection_init_index[1] = collection_of_index[1];
+    collection_init_index[2] = collection_of_index[2];
+
     for(int i = 0; i < numOfClusters; i++) {
         random = rand() % numOfElements;
-        k_means_sepal_len[i] = data_sepal_len_points[random];
-        k_means_sepal_wid[i] = data_sepal_wid_points[random];
-        k_means_petal_len[i] = data_petal_len_points[random];
-        k_means_petal_wid[i] = data_petal_wid_points[random];
+        // k_means_sepal_len[i] = data_sepal_len_points[random];
+        // k_means_sepal_wid[i] = data_sepal_wid_points[random];
+        // k_means_petal_len[i] = data_petal_len_points[random];
+        // k_means_petal_wid[i] = data_petal_wid_points[random];
+        k_means_sepal_len[i] = data_sepal_len_points[collection_init_index[i]];
+        k_means_sepal_wid[i] = data_sepal_wid_points[collection_init_index[i]];
+        k_means_petal_len[i] = data_petal_len_points[collection_init_index[i]];
+        k_means_petal_wid[i] = data_petal_wid_points[collection_init_index[i]];
     }
 
     printf("Running k-means algorithm for %d iterations...\n\n", MAX_ITERATIONS);
@@ -207,11 +281,11 @@ int main(int argc, char *argv[])
     //assign2Cluster(k_means_sepal_len, k_means_sepal_wid, k_means_petal_len, k_means_petal_wid, data_sepal_len_points, data_sepal_wid_points, data_petal_len_points, data_petal_wid_points, k_assignment);
 
     printf("--------------------------------------------------\n");
-    printf("CLUSTERS:\n");
-    for(int i = 0; i < numOfElements; i++)
-    {
-        printf("Point #%d is assigned to Cluster #%d\n", i, k_assignment[i]);
-    }
+    // printf("CLUSTERS:\n");
+    // for(int i = 0; i < numOfElements; i++)
+    // {
+    //     printf("Point #%d is assigned to Cluster #%d\n", i, k_assignment[i]);
+    // }
     printf("RESULTS:");
     int zero = 0;
     int first = 0;
